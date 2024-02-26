68600bb2a6fc4513259f624e9aec89343489040e
FantasySilence <1735793009@qq.com>
2024-02-26 19:14:36 +0800

    添加了线性回归算法的测试

---
+++ test/testRgression/testMultiRegressor.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+test_path = os.path.dirname(current_path)
+root_path = os.path.dirname(test_path)
+sys.path.append(root_path)
+
+from src.common.utilsFile.filesio import FilesIO
+from src.regression.MultiLinearRegression import MultiVarLinearRegression
+
+url = FilesIO.getDataPath("Advertising.csv")
+attribute_list = ['TV', 'radio', 'newspaper']
+target_y = "sales"
+model = MultiVarLinearRegression(url, attribute_list, target_y)
+model.format_output()
---
+++ test/testRgression/testSingleRegressor.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import numpy as np
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+test_path = os.path.dirname(current_path)
+root_path = os.path.dirname(test_path)
+sys.path.append(root_path)
+
+from src.common.utilsFile.filesio import FilesIO
+from src.regression.SinglelinearRegression import SingleVarLinearRegression
+
+
+
+url = FilesIO.getDataPath('datas.csv')
+model = SingleVarLinearRegression(url, 'x', 'ln_y')
+model.one_varLRM()
+model.format_output()
+print()
+print("-"*100)
+xp = np.array([5.5,6.6,7.7])
+model.predict(xp)
+print("-"*100)
+# 可视化
+model.plt_LRM()
+model.regression_diagnostics()
+model.residual_analysis()
