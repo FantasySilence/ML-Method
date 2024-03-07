bda8cec668c2af2315ad15b2d132db007175f828
FantasySilence <1735793009@qq.com>
2024-03-08 01:01:24 +0800

    添加了随机森林算法的测试

---
+++ test/testBoost/testRandomForest_C.py
@@ -0,0 +1,44 @@
+import os 
+import sys
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris, load_wine
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report   
+from sklearn.model_selection import train_test_split
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+test_path = os.path.dirname(current_path)
+root_path = os.path.dirname(test_path)
+sys.path.append(root_path)
+
+from src.algorithm.boost.randomForest_C_R import RandomForestClassifierRegressor
+from src.algorithm.decisiontree.decisionTree_C import DecisionTreeClassifier
+
+
+wine = load_wine()
+iris = load_iris()
+X, y = iris.data, iris.target
+X = StandardScaler().fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42, shuffle=True
+)
+
+base_se = DecisionTreeClassifier(max_bins=50, max_depth=10, is_all_feature_R=True)
+rf_model = RandomForestClassifierRegressor(
+    base_estimator=base_se, n_estimators=20, task="c",
+    OOB=True, feature_importance=True
+)
+rf_model.fit(X_train, y_train)
+y_hat = rf_model.predict(X_test)
+print(classification_report(y_test, y_hat))
+print("包外估计的精度：", rf_model.oob_score)
+print("特征重要性：", rf_model.feature_importance_scores)
+
+plt.figure(figsize=(10, 6))
+data_iris = pd.DataFrame([iris.feature_names, rf_model.feature_importance_scores]).T
+data_iris.columns = ["Feature Name", "Importance"]
+sns.barplot(x="Importance", y="Feature Name", data=data_iris)
+plt.title("Iris DataSet Feature Importance", fontdict={"fontsize":14})
+plt.show()
---
+++ test/testBoost/testRandomForest_R.py
@@ -0,0 +1,56 @@
+import os 
+import sys
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+test_path = os.path.dirname(current_path)
+root_path = os.path.dirname(test_path)
+sys.path.append(root_path)
+
+from src.common.utilsFile.filesio import FilesIO
+from src.algorithm.boost.randomForest_C_R import RandomForestClassifierRegressor
+from src.algorithm.decisiontree.decisionTree_R import DecisionTreeRegressor
+
+
+boston = pd.read_csv(FilesIO.getDataPath("Boston.csv"))
+X, y = boston.iloc[:, 1:-1].values, boston.iloc[:, -1].values
+X = StandardScaler().fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+base_es = DecisionTreeRegressor(max_bins=50, max_depth=10)
+rf_model = RandomForestClassifierRegressor(
+    base_estimator=base_es, n_estimators=50, task="r", 
+    OOB=True, feature_sampling_rate=0.3, feature_importance=True
+)
+rf_model.fit(X_train, y_train)
+idx = np.argsort(y_test)
+y_pred = rf_model.predict(X_test)
+
+plt.figure(figsize=(10, 6))
+plt.plot(y_test[idx], "k-", lw=1.5, label="Test True Values")
+plt.plot(y_pred[idx], "r-", lw=1, label="Predict Values")
+plt.title(
+    "Boston House (R2 = %.5f, MSE = %.5f)"%(
+        r2_score(y_test, y_pred), ((y_test - y_pred) ** 2).mean()
+    )
+)
+plt.xlabel("Observation Serial Number", fontdict={"fontsize":12})
+plt.ylabel("Test True VS Predict Values", fontdict={"fontsize":12})
+plt.legend(frameon=False)
+plt.grid(ls=":")
+plt.show()
+
+plt.figure(figsize=(10, 6))
+data_boston = pd.DataFrame([boston.columns[1:-1], rf_model.feature_importance_scores]).T
+data_boston.columns = ["Feature Name", "Importance"]
+sns.barplot(x="Importance", y="Feature Name", data=data_boston)
+plt.title("Boston DataSet Feature Importance", fontdict={"fontsize":14})
+plt.show()
