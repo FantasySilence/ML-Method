f2aab7ad745a57645e20c1c74effc047cfa4f050
FantasySilence <1735793009@qq.com>
2024-03-09 20:10:07 +0800

    添加了主程序入口便于调用各个模块解决问题

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+-----------------------------------------
+Author: @Fantasy_Silence
+Time: 2024-03-09
+IDE: Visual Studio Code  Python: 3.9.7
+-----------------------------------------
+这是主程序入口，在这里同一调用各个模块解决问题
+"""
+
+import pandas as pd
+from src.common.utilsFile.filesio import FilesIO
+from src.algorithm.regression.multiLinearRegression import MultiVarLinearRegression
+
+url = FilesIO.getDataPath("Boston.csv")
+data = pd.read_csv(url)
+attribute_list, target_y = data.columns[1:-1].to_list(), data.columns[-1]
+model = MultiVarLinearRegression(url, attribute_list, target_y)
+model.format_output()
